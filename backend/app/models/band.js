cc98525ba000bca517ebf14998158ddfa1123a86
Lukas Grimm <ombre@ombre.ch>
2016-10-13 09:09:57 +0200

    added band model

---
+++ backend/app/models/band.js
@@ -0,0 +1,19 @@
+// Band model
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var BandSchema = new Schema({
+  id: String,
+  name: String,
+  contact: String,
+  raider: { type: Schema.Typers.ObjectId, ref: 'Raider' }
+});
+
+BandSchema.virtual('date')
+  .get(function(){
+    return this._id.getTimestamp();
+  });
+
+mongoose.model('Band', BandSchema);
+
