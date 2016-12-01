ce9ab2d298f0eae3c4b3aae4f861e7663e87083f
Lukas Grimm <grimm@puzzle.ch>
2016-12-01 20:41:47 +0100

    added packages

--- backend/config/express.js
+++ backend/config/express.js
@@ -12,7 +12,7 @@ module.exports = function(app, config) {
   var env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
-  
+
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'jade');
 
@@ -32,12 +32,17 @@ module.exports = function(app, config) {
     require(controller)(app);
   });
 
+  var apis = glob.sync(config.root + '/app/endpoints/*.js');
+  controllers.forEach(function (api) {
+    require(api)(app);
+  });
+
   app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
-  
+
   if(app.get('env') === 'development'){
     app.use(function (err, req, res, next) {
       res.status(err.status || 500);
